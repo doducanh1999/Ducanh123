3393887d021a4897a7298fa39e69ab4c03d6859c
doducanh1999 <anhddgch17484@fpt.edu.vn>
2019-11-26 11:55:10 +0700

    h

---
+++ view.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php 
+$db = parse_url(getenv("DATABASE_URL"));
+
+$pdo = new PDO("pgsql:" . sprintf(
+    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+    $db["host"],
+    $db["port"],
+    $db["user"],
+    $db["pass"],
+    ltrim($db["path"], "/")
+));
+	$sql = "select * from product";
+	//compile the sql
+	$stmt = $pdo->prepare($sql);
+	//execute the query on the server and return the result set
+	$stmt->setFetchMode(PDO::FETCH_ASSOC);
+	$stmt->execute();
+	$resultSet = $stmt->fetchAll();
+	 ?>
+	 <ul>
+	 	<?php 
+	 	foreach ($resultSet as $row) {
+	 		echo "<li>".
+	 		$row["name"] . '--' . $row["price"]
+	 		."</li>";
+	 	}
+	 	 ?>
+	 </ul>
+</body>
+</html>
