5ca470c4eb2ec5430c35883f11bbeaea4f359b1f
doducanh1999 <anhddgch17484@fpt.edu.vn>
2019-11-28 09:32:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,15 +4,13 @@
 	<title>Create Product</title>
 
 <body>
-	<form>
-		action="insertProduct.php" method="post"
-	</form>
+	<form action="insertProduct.php" method="post">
 
 Name <input type="text" name="name">
 <br>
 Price <input type="text" name="price">
 <br>
 <input type="submit" value="Insert">
-
+</form>
 </body>
 </html>
