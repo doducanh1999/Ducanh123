62f4e1df7720dcdbb393a8c4f13a96874008692e
doducanh1999 <anhddgch17484@fpt.edu.vn>
2019-11-28 09:29:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Create Product</title>
 
-	<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-
-<!-- jQuery library -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
-<!-- Popper JS -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-
-<!-- Latest compiled JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-
-</head>
 <body>
+	<form>
+		action="insertProduct.php" method="post"
+	</form>
 
-<div class="jumbotron text-center">
-	<h1 style="color: red;size: 100">Hello my name is Dat</h1>
-</div>
-<a href="view.php" class="col-sm-4" style="">View profile</a>
+Name <input type="text" name="name">
+<br>
+Price <input type="text" name="price">
+<br>
+<input type="submit" value="Insert">
+
 </body>
 </html>
