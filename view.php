cc2de32252437dae0d555df0a9c1abb2898790c6
doducanh1999 <anhddgch17484@fpt.edu.vn>
2019-11-28 11:31:27 +0700

    Update view.php

--- view.php
+++ view.php
@@ -28,7 +28,7 @@ $pdo = new PDO("pgsql:" . sprintf(
 	 	<?php
             foreach ($resultSet as $row) {
                 echo "<li>" .
-                 '<a href="delete.php?id=' . $row["name"] .  '">' .   $row["name"] 
+                 '<a href="delete.php?id=' . $row["id"] .  '">' .   $row["name"] 
                         . '--'. $row["price"] 
                 . '</a>'
                 . "</li>";
